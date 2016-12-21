f4d57b418f407a9c52b5c1deeaab3c695f00eb26
Devon Harris <dbharris2@gmail.com>
2016-12-21 13:00:33 -0800

    [dnd] Make modal scrollable

--- public/scripts/dnd_container.jsx
+++ public/scripts/dnd_container.jsx
@@ -145,13 +145,15 @@ export default class DNDContainer extends React.Component {
   render() {
     const customStyles = {
       content : {
-        top                   : '50%',
-        left                  : '50%',
-        right                 : 'auto',
-        bottom                : 'auto',
-        marginRight           : '-50%',
-        transform             : 'translate(-50%, -50%)',
-        maxWidth              : '800px'
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        maxWidth: '800px',
+        maxHeight: '80%',
+        overflow: 'scroll',
       }
     };
 
