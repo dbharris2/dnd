363280445c335d28b5280c26192b1dc57824663b
Devon Harris <dbharris2@gmail.com>
2017-01-18 19:54:46 -0800

    [dnd] Enable material-ui by adding a theme provider

--- public/scripts/main.jsx
+++ public/scripts/main.jsx
@@ -2,10 +2,17 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import DNDContainer from './dnd_container';
 
+const App = () => (
+  <MuiThemeProvider>
+    <DNDContainer />
+  </MuiThemeProvider>
+);
+
 ReactDOM.render(
-  <DNDContainer />,
+  <App />,
   document.getElementById('content')
 );
